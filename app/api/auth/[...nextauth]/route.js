47eb472552b80fd25ee528dd1ca4a8dfefe5b943
kongmenglor <lorxx504@umn.edu>
2023-06-22 14:54:55 -0500

    updates

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,11 +1,17 @@
 import NextAuth from "next-auth/next";
 import GoogleProvider from 'next-auth/providers/google'
 
+console.log(
+    "ID: " + process.env.GOOGLE_ID,
+    " /Secret: " + process.env.GOOGLE_CLIENT_SECRET
+)
+
+
 const handler = NextAuth({
     providers: [
         GoogleProvider({
-            clientId: "",
-            clientSecret: ""
+            clientId: process.env.GOOGLE_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
         })
     ], async session({session}) {
 
@@ -15,4 +21,4 @@ const handler = NextAuth({
     }
 })
 
-export {handler as GET, hander as POST}
+export {handler as GET, handler as POST};
