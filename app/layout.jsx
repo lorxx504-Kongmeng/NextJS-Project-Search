4c7ceeeeb5037540708f0187ef3f348a760d7672
kongmenglor <lorxx504@umn.edu>
2023-06-22 14:46:53 -0500

    working on provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -10,15 +10,17 @@ export const metadata = {
 const RootLayout = ({children}) => {
   return (
     <html lang='en'>
-        <body>
-            <div className='main'>
-                <div className='gradient'/>
-            </div>
-            <main className='app'>
-                <Nav />
-                {children}
-            </main>
-        </body>
+            <body>
+            <Provider>
+                <div className='main'>
+                    <div className='gradient'/>
+                </div>
+                <main className='app'>
+                    <Nav />
+                    {children}
+                </main>
+            </Provider>
+            </body> 
     </html>
   )
 }
