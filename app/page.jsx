43c813caf5a225abbf58d628e8ffd6ac191590ff
kongmenglor <lorxx504@umn.edu>
2023-06-22 13:10:53 -0500

    working on nav bar

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,23 @@
+import Feed from '@components/Feed'
+
+
+
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+        <h1 className="head_text text-center">Discover & Share
+        <br></br>
+        <span className="orange_gradient">
+        AI-Powered Prompts
+        </span>
+        </h1>
+        <p>
+            Promptopia is an open-source AI prompting tool for modern world to discover, create and share creative prompts
+        </p>
+        <Feed />
+    </section>
+
   )
 }
 
